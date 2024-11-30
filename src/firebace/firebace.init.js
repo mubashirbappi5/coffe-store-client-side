a34f0bab2094e521d7c156b29599a68a5ef45781
mubashirbappi <mubashirbappi@gmail.com>
2024-12-01 02:01:00 +0600

    error handle

--- src/firebace/firebace.init.js
+++ src/firebace/firebace.init.js
@@ -5,14 +5,16 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-};
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDnWq6mQdgHIYr0el0gwAEkgbeei2Y8c3w",
+    authDomain: "coffee-shop-9e0b1.firebaseapp.com",
+    projectId: "coffee-shop-9e0b1",
+    storageBucket: "coffee-shop-9e0b1.firebasestorage.app",
+    messagingSenderId: "33847546383",
+    appId: "1:33847546383:web:4a5e11dc2460c06d06897a"
+  };
+
 
 
 // Initialize Firebase
